154b9a5bf27e0bfe8108cbb62997adef579793b4
RIBANAG <131253682+RIBANAG@users.noreply.github.com>
2023-05-20 20:19:19 -0300

    Create Creacion_bd.sql
    Script de nuestra base de datos.

---
+++ Creacion_bd.sql
@@ -0,0 +1,26 @@
+create database big_bread_sa;
+use big_bread_sa;
+CREATE TABLE productos (
+  id INT PRIMARY KEY ,
+  nombre VARCHAR(100) NOT NULL,
+  descripcion VARCHAR(255),
+  precio DECIMAL(10, 2) NOT NULL
+);
+CREATE TABLE insumos (
+  id INT PRIMARY KEY ,
+  nombre VARCHAR(100) NOT NULL,
+  descripcion VARCHAR(255)
+);
+CREATE TABLE produccion_diaria (
+  id INT PRIMARY KEY ,
+  fecha DATE NOT NULL,
+  cantidad INT NOT NULL
+);
+CREATE TABLE recetas (
+  id INT PRIMARY KEY ,
+  producto_id INT NOT NULL,
+  insumo_id INT NOT NULL,
+  cantidad DECIMAL(10, 2) NOT NULL,
+  FOREIGN KEY (producto_id) REFERENCES productos(id),
+  FOREIGN KEY (insumo_id) REFERENCES insumos(id)
+);
