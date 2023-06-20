3987a80dfdc0391baa8e9ed6ee4fe6aef33cc0a3
Federico Guzman <112135630+fedeguzani@users.noreply.github.com>
2023-06-19 22:34:54 -0300

    Update produccion_diaria.py

--- produccion_diaria.py
+++ produccion_diaria.py
@@ -1,11 +1,12 @@
 
+
 class ProduccionDiaria:
     def __init__(self, conexion):
         self.conexion = conexion
 
     def insertar_produccion(self, fecha, cantidad):
-        query = "INSERT INTO produccion_diaria ( fecha, cantidad) VALUES ( %s, %s)"
-        params = ( fecha, cantidad)
+        query = "INSERT INTO produccion_diaria (fecha, cantidad) VALUES (%s, %s)"
+        params = (fecha, cantidad)
         return self.conexion.ejecutar_consulta(query, params)
 
     def editar_produccion(self, id, fecha, cantidad):
@@ -21,8 +22,12 @@ class ProduccionDiaria:
     def listar_produccion(self):
         query = "SELECT * FROM produccion_diaria"
         return self.conexion.obtener_filas(query)
+    
+    def obtener_produccion_total_dia(self, fecha):
+        query = "SELECT SUM(cantidad) FROM produccion_diaria WHERE fecha = %s"
+        self.conexion.cursor.execute(query, (fecha,))
+        total = self.conexion.cursor.fetchone()[0]
+        return total
 
     def cerrar_conexion(self):
         self.conexion.cerrar_conexion()
-    
-    
